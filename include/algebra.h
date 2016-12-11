59aed45d006b2adb4418f4d9637645328ff1c09a
Wesley Smith <wesley.hoke@gmail.com>
2016-12-10 17:08:23 -0800

    formatting

--- include/algebra.h
+++ include/algebra.h
@@ -1,35 +1,31 @@
 #ifndef SPACE_ALGEBRA_H
 #define SPACE_ALGEBRA_H
 
-#include "multivector.h"
 #include "basis.h"
+#include "multivector.h"
 
-namespace space
-{
-	template<class Metric_, class ScalarValue_>
-	struct Algebra
-	{
-		using Metric = Metric_;
-		using ScalarValue = ScalarValue_;
-		static constexpr auto Dim = Metric::Dim;
-	
-		template<class Basis>
-		using Multivector = Multivector<Algebra, Basis>;
+namespace space {
+    template <class Metric_, class ScalarValue_>
+    struct Algebra {
+        using Metric = Metric_;
+        using ScalarValue = ScalarValue_;
+        static constexpr auto Dim = Metric::Dim;
 
-		
-		using ScalarBasis = basis::Scalar<Algebra>;
-		using VectorBasis = basis::Vectors<Algebra>;
-		using PseudoScalarBasis = basis::PseudoScalar<Algebra>;
-		
-		using Scalar = Multivector<ScalarBasis>;
-		using Vec = Multivector<VectorBasis>;
-		using PseudoScalar = Multivector<PseudoScalarBasis>;
-		
-		
-		// Aliases
-		using S = Scalar;
-		using Pss = PseudoScalar;
-	};
+        template <class Basis>
+        using Multivector = Multivector<Algebra, Basis>;
+
+        using ScalarBasis = basis::Scalar<Algebra>;
+        using VectorBasis = basis::Vectors<Algebra>;
+        using PseudoScalarBasis = basis::PseudoScalar<Algebra>;
+
+        using Scalar = Multivector<ScalarBasis>;
+        using Vec = Multivector<VectorBasis>;
+        using PseudoScalar = Multivector<PseudoScalarBasis>;
+
+        // Aliases
+        using S = Scalar;
+        using Pss = PseudoScalar;
+    };
 }
 
 #endif
