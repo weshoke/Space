b1fac8558f0ee0b602d98e7a65daff6bb7026a74
Wesley Smith <wesley.hoke@gmail.com>
2016-12-30 09:33:04 -0500

    conformal tests

--- test/blade_product_bit_product.cpp
+++ test/blade_product_bit_product.cpp
@@ -25,54 +25,46 @@ static_assert(BitProductBlade<ME3, 0b101, 0b01>::Scale<float>() == -1.f, "BitPro
 static_assert(BitProductBlade<ME3, 0b101, 0b10>::Scale<float>() == -1.f, "BitProduct error");
 static_assert(BitProductBlade<ME3, 0b101, 0b100>::Scale<float>() == 1.f, "BitProduct error");
 
+// clang-format off
 using MC2 = space::Metric<3, 1, space::basis::xform::Conformal<2, 3>>;
 // e1 * e1
 static_assert(brigand::size<BitProduct<MC2, 0b01, 0b01>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b01, 0b01>>::value == 0b00, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b01, 0b01>>::Scale<float>() == 1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b01, 0b01>>::Scale<float>() == 1.f, "BitProduct error");
 // e1 * e2
 static_assert(brigand::size<BitProduct<MC2, 0b01, 0b10>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b01, 0b10>>::value == 0b11, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b01, 0b10>>::Scale<float>() == 1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b01, 0b10>>::Scale<float>() == 1.f, "BitProduct error");
 // e1 * ori
 static_assert(brigand::size<BitProduct<MC2, 0b001, 0b100>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b001, 0b100>>::value == 0b101, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b001, 0b100>>::Scale<float>() == 1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b001, 0b100>>::Scale<float>() == 1.f, "BitProduct error");
 // e1 * inf
 static_assert(brigand::size<BitProduct<MC2, 0b0001, 0b1000>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b0001, 0b1000>>::value == 0b1001, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b0001, 0b1000>>::Scale<float>() == 1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b0001, 0b1000>>::Scale<float>() == 1.f, "BitProduct error");
 // (e1 ^ ori) * e1
 static_assert(brigand::size<BitProduct<MC2, 0b0101, 0b0001>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b0101, 0b0001>>::value == 0b0100, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b0101, 0b0001>>::Scale<float>() == -1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b0101, 0b0001>>::Scale<float>() == -1.f, "BitProduct error");
 // (e1 ^ ori) * ori
 static_assert(brigand::size<BitProduct<MC2, 0b0101, 0b0100>>::value == 0, "BitProduct error");
 // (e1 ^ inf) * e1
 static_assert(brigand::size<BitProduct<MC2, 0b1001, 0b0001>>::value == 1, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b1001, 0b0001>>::value == 0b1000, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b1001, 0b0001>>::Scale<float>() == -1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b1001, 0b0001>>::Scale<float>() == -1.f, "BitProduct error");
 // (e1 ^ inf) * inf
 static_assert(brigand::size<BitProduct<MC2, 0b1001, 0b1000>>::value == 0, "BitProduct error");
 // ori * inf
 static_assert(brigand::size<BitProduct<MC2, 0b0100, 0b1000>>::value == 2, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b0100, 0b1000>>::value == 0b0000, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b0100, 0b1000>>::Scale<float>() == -1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b0100, 0b1000>>::Scale<float>() == -1.f, "BitProduct error");
 static_assert(brigand::back<BitProduct<MC2, 0b0100, 0b1000>>::value == 0b1100, "BitProduct error");
-static_assert(brigand::back<BitProduct<MC2, 0b0100, 0b1000>>::Scale<float>() == 1.f,
-              "BitProduct error");
+static_assert(brigand::back<BitProduct<MC2, 0b0100, 0b1000>>::Scale<float>() == 1.f, "BitProduct error");
 // inf * ori
 static_assert(brigand::size<BitProduct<MC2, 0b1000, 0b0100>>::value == 2, "BitProduct error");
 static_assert(brigand::front<BitProduct<MC2, 0b1000, 0b0100>>::value == 0b0000, "BitProduct error");
-static_assert(brigand::front<BitProduct<MC2, 0b1000, 0b0100>>::Scale<float>() == -1.f,
-              "BitProduct error");
+static_assert(brigand::front<BitProduct<MC2, 0b1000, 0b0100>>::Scale<float>() == -1.f, "BitProduct error");
 static_assert(brigand::back<BitProduct<MC2, 0b1000, 0b0100>>::value == 0b1100, "BitProduct error");
-static_assert(brigand::back<BitProduct<MC2, 0b1000, 0b0100>>::Scale<float>() == -1.f,
-              "BitProduct error");
+static_assert(brigand::back<BitProduct<MC2, 0b1000, 0b0100>>::Scale<float>() == -1.f, "BitProduct error");
+// clang-format on
